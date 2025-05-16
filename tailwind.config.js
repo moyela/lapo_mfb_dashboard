18b62f5e0d9d6413f6fb592844e9770064af6e13
Wemi Moyela <wemimoyela@gmail.com>
2025-05-16 19:04:52 +0100

    fixing deployment bug through tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './public/**/*.html', 
+    './src/**/*.{js,jsx,ts,tsx,vue}', 
+    './pages/**/*.{js,ts,jsx,tsx,mdx}', 
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
