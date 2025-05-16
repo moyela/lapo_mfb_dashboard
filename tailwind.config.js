ea9676f95fe6c9a308aac9618618bde940612903
Wemi Moyela <wemimoyela@gmail.com>
2025-05-16 19:21:31 +0100

    Revert "fixing deployment bug through tailwind config"
    This reverts commit 18b62f5e0d9d6413f6fb592844e9770064af6e13.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './public/**/*.html', 
-    './src/**/*.{js,jsx,ts,tsx,vue}', 
-    './pages/**/*.{js,ts,jsx,tsx,mdx}', 
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
